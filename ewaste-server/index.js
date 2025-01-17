c34478035568c786b449c964961adcc429ec0b03
namanchauhan16 <86224221+namanchauhan16@users.noreply.github.com>
2025-01-17 19:57:51 +0530

    Update index.js

--- ewaste-server/index.js
+++ ewaste-server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors(
   {
     origin: ["https://scrapster-frontend.vercel.app"],
-    methods: ["POST", "GET"],
+    methods: ["post", "get"],
     credentials: true
   }
 ));
