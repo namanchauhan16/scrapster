18287b02c6607e5cfcf0c9518a8867ff0c6acab9
namanchauhan16 <namanchauhan59@gmail.com>
2025-01-18 16:46:23 +0530

    .

--- ewaste-server/index.js
+++ ewaste-server/index.js
@@ -17,7 +17,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// app.options('*', cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
